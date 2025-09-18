01047a2ad8689bff083a9e6e737435d189c05f3b
Matt Sikes <emsikes@gmail.com>
2025-09-17 18:10:41 -0700

    ecommerce-site backend initial commit

---
+++ main-projects/ecommerce-site/backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
+
+    // Confirm all requried fields are present before created new product item
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success:false, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.log("Error in Create product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+app.delete("api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    console.log("id:", id)
+})
+
+app.listen(5001, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5001");
+})
